40fcf829f6a88fe374ed245d31e2a26b924181a8
Denis Gukov <denguk@gmail.com>
2025-05-08 15:30:19 +0500

    feat: add terragrunt

--- demo.tf
+++ demo.tf
@@ -9,13 +9,19 @@ terraform {
 
 provider "local" {}
 
-# Define a local file
-resource "local_file" "example" {
-  filename = "${path.module}/example.txt"
-  content  = <<EOT
+variable "file_content" {
+  description = "The content to write to the local file"
+  type        = string
+  default     = <<EOT
 This is an example file created by Terraform.
 You can customize its content.
 EOT
+}
+
+# Define a local file
+resource "local_file" "example" {
+  filename = "${path.module}/example.txt"
+  content  = var.file_content
 }
 
 # Output the file path
---
+++ terragrunt.hcl
@@ -0,0 +1,7 @@
+terraform {
+  source = "./"
+}
+
+inputs = {
+  file_content = "This file was written using Terragrunt + local_file."
+}
